09d649a77f6c26d62f4f57d22c9f95679af932fd
Roman Akhmedianov <rodger@elewise.com>
2018-05-14 16:14:41 +0400

    Первый коммит

---
+++ ELMA.EleWise.Calc/ELMA.EleWise.Calc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELMA.EleWise.Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
