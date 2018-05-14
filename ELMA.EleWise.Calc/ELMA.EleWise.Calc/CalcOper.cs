058b8d8537c03de9ee6406ead32844a27b8f8afd
Roman Akhmedianov <rodger@elewise.com>
2018-05-14 16:44:50 +0400

    Операция умножения

---
+++ ELMA.EleWise.Calc/ELMA.EleWise.Calc/CalcOper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELMA.EleWise.Calc
+{
+    public enum CalcOper
+    {
+        Sum,
+        Mult
+    }
+}
--- ELMA.EleWise.Calc/ELMA.EleWise.Calc/Program.cs
+++ ELMA.EleWise.Calc/ELMA.EleWise.Calc/Program.cs
@@ -10,12 +10,27 @@ namespace ELMA.EleWise.Calc
     {
         static void Main(string[] args)
         {
-            if (args.Length != 2)
+            if (args.Length != 3)
             {
                 Console.WriteLine("Не указаны аргументы");
                 return;
             }
-            Console.WriteLine(Convert.ToInt64(args[0]) + Convert.ToInt64(args[1]));
+            CalcOper oper;
+            if (!Enum.TryParse(args[0], out oper))
+            {
+                Console.WriteLine("Не указана операция");
+                return;
+            }
+            switch (oper)
+            {
+                case CalcOper.Sum:
+                    Console.WriteLine(Convert.ToInt64(args[1]) + Convert.ToInt64(args[2]));
+                    break;
+                case CalcOper.Mult:
+                    Console.WriteLine(Convert.ToInt64(args[1]) * Convert.ToInt64(args[2]));
+                    break;
+            }
+           
             Console.ReadKey();
         }
     }
